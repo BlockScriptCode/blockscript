dee46a76645b5356466ae268f4761b9e531ab822
heschwerdt <h.schwerdtner@tu-berlin.de>
2022-10-29 12:45:40 +0200

    my commit

---
+++ src/bs.h
@@ -0,0 +1,22 @@
+#ifndef bs_h
+#define bs_h
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define BS_VERSION_MAJOR "1"
+#define BS_VERSION_MINOR "0"
+#define BS_VERSION_RELEASE "0"
+
+#define BS_VERSION "Bs " BS_VERSION_MAJOR "." BS_VERSION_MINOR "." BS_VERSION_RELEASE
+
+#define BS_AUTHORS "H. Schwerdtner"
+
+#define BS_SIGNATURE "\x1brbs"
+
+
+
+
+#endif
