c3677e1b80f1b7386d70208d4bc6c0e2f56da0f3
heschwerdt <h.schwerdtner@tu-berlin.de>
2022-11-18 20:05:23 +0100

    updated syntax tree

--- src/bs_ast.h
+++ src/bs_ast.h
@@ -20,19 +20,30 @@ typedef enum
     // literals
     LITERAL,
     IDENTIFIER,
-    // unarys
-    UNARY_EXPRESSION,
-    // binarys
-    BINARY_EXPRESSION,
-    // tenary
-    CONDITIONAL_EXPRESSION,
 
+    // Expressions
+    UNARY_EXPRESSION,
+    BINARY_EXPRESSION,
+    CONDITIONAL_EXPRESSION,
+    ASSIGNMENT,
+    FN,
+
+    // Statements
+    FOR_STMT,
+    IF_STMT,
+    RETURN_STMT,
+    WHILE_STMT,
+    BLOCK_STMT,
     EXPRESSION_STATEMENT,
 
+    // Declarations
+    CLASS_DECLARATION,
+    FN_DECLARATION,
     VARIABLE_DECLARATOR,
     VARIABLE_DECLARATION,
 
-    BLOCK_STMT,
+    // Program
+    PROGRAM,
 } ast_type;
 
 typedef enum
@@ -104,17 +115,70 @@ struct AST
             AST *id;
             AST *init;
         } VARIABLE_DECLARATOR;
+        struct FN
+        {
+            AST *id;
+            int paramenterCount;
+            AST **parameters;
+            AST *body;
+        } FN;
+        // Declaration
         struct VARIABLE_DECLARATION
         {
             AST *declaration;
             ast_variable_kind kind;
             bs_value_type type;
         } VARIABLE_DECLARATION;
+        struct FN_DECLARATION
+        {
+            AST *fn;
+        } FN_DECLARATION;
+        struct CLASS_DECLARATION
+        {
+            AST *name;
+            AST *extends;
+            int attributeCount;
+            AST **attributes;
+            int functionCount;
+            AST **functions;
+        } CLASS_DECLARATION;
+        // Statements
+        struct EXPR_STMT
+        {
+            AST *expression;
+        } EXPR_STMT;
+        struct FOR_STMT
+        {
+            AST *init;
+            AST *test;
+            AST *update;
+            AST *body;
+        } FOR_STMT;
+        struct IF_STMT
+        {
+            AST *test;
+            AST *consequent;
+            AST *alternate;
+        } IF_STMT;
+        struct RETURN_STMT
+        {
+            AST *argument;
+        } RETURN_STMT;
+        struct WHILE_STMT
+        {
+            AST *test;
+            AST *body;
+        } WHILE_STMT;
         struct BLOCK_STMT
         {
             int instructionCount;
             AST **instructions;
         } BLOCK_STMT;
+        struct PROGRAM
+        {
+            int instructionCount;
+            AST **instructions;
+        } PROGRAM;
     } data;
 };
 
